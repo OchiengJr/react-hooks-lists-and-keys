000880bf7afb669768180ddbd5f7fae14a37be58
OchiengJr <chrispine.ochieng@student.moringaschool.com>
2023-12-10 14:20:01 +0300

    Lists and Keys

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,30 +1,19 @@
 import React from "react";
 
 function ColorList() {
-  const colors = [
-    "firebrick",
-    "rebeccapurple",
-    "salmon",
-    "darkslategray",
-    "hotpink",
-  ];
+  // List of top 5 CSS colors
+  const colors = ["firebrick", "rebeccapurple", "salmon", "darkslategray", "hotpink"];
 
-  const colorElements = colors.map((color) => {
-    return <li key={color} style={{color: color}}>{color}</li>
-  })
+  // Create list items for each color using the map function
+  const colorElements = colors.map((color) => (
+    <li key={color} style={{ color }}>{color}</li>
+  ));
 
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
-      {/* <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
-      </ol> */}
-
       <ol>
+        {/* Render the list items */}
         {colorElements}
       </ol>
     </div>
@@ -32,3 +21,4 @@ function ColorList() {
 }
 
 export default ColorList;
+
